85b4d614ccbe90f408608412205a14930937f764
allison ruiz <allisonruiz@allisons-MacBook-Pro.local>
2022-02-02 19:12:43 -0800

    index.js

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ let dogImages = document.querySelectorAll('.dog-image')
 for(let i = 0; i < dogImages.length; i++){
     dogImages[i].style.borderRadius = '50px'
 }
+let header = document.querySelector('#page-header')
+header.style.fontColor = "pink"
+
+let background = document.querySelector('#background-color')
+background.style.color = "#cdebf9"
